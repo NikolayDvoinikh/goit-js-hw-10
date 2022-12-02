fcf6ec5e631501ade393c63c025481415b72e26e
NikolayDvoinikh <nikolaydvoinikh@gmail.com>
2022-12-02 14:49:02 +0200

    update

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,16 +1,25 @@
-import { refs } from './index';
-
 const COUNTRIES_URL = 'https://restcountries.com/v3.1/name/';
 
 export function fetchCountries(name) {
-  name = refs.inputField.value;
   console.log(name);
-  fetch(`${COUNTRIES_URL}${name}`)
-    .then(res => res.json())
-    .then(data =>
-      data.map(({ name, capital, population, flags, languages }) =>
-        console.log(name.official, capital, population, flags.svg, languages)
-      )
-    )
-    .catch(error => console.log(error));
+  fetch(
+    `${COUNTRIES_URL}${name}?fields=name,capital,population,flags,languages`
+  ).then(res => {
+    if (!res.ok) {
+      throw new Error(res.status);
+    }
+    return res.json();
+  });
 }
+
+// const URL = "https://restcountries.com/v3.1/name/"
+
+// export function fetchCountries(name) {
+//     return fetch(`${URL}${name}?fields=name,capital,population,flags,languages`)
+//         .then(r => {
+//             if (!r.ok || r.status === 404) {
+//                 throw new Error(r.status);
+//             }
+//             return r.json();
+//         });
+// }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,99 @@
 import './css/styles.css';
 import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
 import { fetchCountries } from './fetchCountries.js';
 export { refs };
 const DEBOUNCE_DELAY = 1300;
 
 const refs = {
   inputField: document.querySelector('input#search-box'),
+  countryList: document.querySelector('ul.country-list'),
+  countryInfo: document.querySelector('div.country-info'),
 };
+
+let items = [];
 
 refs.inputField.addEventListener(
   'input',
-  debounce(fetchCountries, DEBOUNCE_DELAY)
+  debounce(onInputSearch, DEBOUNCE_DELAY)
 );
+
+function onInputSearch(e) {
+  let countryName = e.target.value.trim();
+  fetchCountries(countryName)
+    .then(renderList())
+    .catch(err => console.log(err));
+}
+
+// const refs = {
+//   seachField: document.querySelector('#search-box'),
+//   countryList: document.querySelector('.country-list'),
+//   countryInfo: document.querySelector('.country-info'),
+// };
+
+// refs.seachField.addEventListener(
+//   'input',
+//   debounce(onSearchInput, DEBOUNCE_DELAY)
+// );
+
+// function onSearchInput(e) {
+//   const countryName = e.target.value.trim();
+
+//   if (countryName === '') {
+//     clearContent();
+//     return;
+//   }
+
+//   fetchCountries(countryName)
+//     .then(onTermsCheck)
+//     .catch(error => {
+//       Notiflix.Notify.failure('Oops, there is no country with that name');
+//       clearContent();
+//       return console.log(error);
+//     });
+// }
+
+// function onTermsCheck(country) {
+//   if (country.length > 10) {
+//     Notiflix.Notify.info(
+//       'Too many matches found. Please enter a more specific name.'
+//     );
+//     clearContent();
+//     return;
+//   } else if (country.length === 1) {
+//     clearContent(refs.countryList.innerHTML);
+//     renderCountryInfo(country);
+//   } else if (country.length > 1 && country.length <= 10) {
+//     clearContent(refs.countryInfo.innerHTML);
+//     renderCountryList(country);
+//   }
+// }
+
+// const renderCountryList = country => {
+//   const markup = country
+//     .map(({ name, flags }) => {
+//       return `<li><img src="${flags.svg}" alt="Flag of ${name.official}"
+//             width="30" hight="20"><b>${name.official}</p></li>`;
+//     })
+//     .join('');
+
+//   refs.countryList.innerHTML = markup;
+// };
+
+// const renderCountryInfo = country => {
+//   refs.countryInfo.innerHTML = country
+//     .map(({ name, capital, population, flags, languages }) => {
+//       return `<section><h1><img src="${flags.svg}" alt="${
+//         name.official
+//       }" width="100" height="60">&nbsp ${name.official}</h1>
+//       <p><span>Capital: </span>&nbsp ${capital}</p>
+//       <p><span>Population:</span>&nbsp ${population}</p>
+//       <p><span>Languages:</span>&nbsp ${Object.values(languages)}</p><section>`;
+//     })
+//     .join('');
+// };
+
+// function clearContent() {
+//   refs.countryList.innerHTML = '';
+//   refs.countryInfo.innerHTML = '';
+// }
